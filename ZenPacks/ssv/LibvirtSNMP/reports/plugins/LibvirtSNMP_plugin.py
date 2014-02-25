9c319c4791e04d7595b2c7dcf2425f65d22ce107
sergevs <abricus@yandex.ru>
2014-02-25 20:48:13 +0400

    bug fixes and improvements

--- ZenPacks/ssv/LibvirtSNMP/reports/plugins/LibvirtSNMP_plugin.py
+++ ZenPacks/ssv/LibvirtSNMP/reports/plugins/LibvirtSNMP_plugin.py
@@ -12,16 +12,10 @@ class  LibvirtSNMP_plugin(AliasPlugin):
     # The run method will be executed when your report calls the plugin.
     def run(self, dmd, args):
         report = []
-        searchHost = args.get('searchHost', '') or ''
         for device in dmd.Devices.getSubDevicesGen():
             if device.os.libvirtsnmp():
-                for lcomp in device.os.libvirtsnmp():
+                for lcomp in sorted( device.os.libvirtsnmp(), key=lambda x: x.guestName ):
                   guestUrl=dmd.Devices.findDevice(lcomp.guestName).getPrimaryUrlPath() if dmd.Devices.findDevice(lcomp.guestName) else ''
-                  if searchHost:
-                    if re.match(searchHost, lcomp.title): 
-                      log.info(searchHost)
-                      report.append(Record(  device=device, guestName = lcomp.guestName, guestUrl = guestUrl, guestCpuCount = lcomp.guestCpuCount, guestMemory = lcomp.guestMemoryCurrent)) 
-                  else:
-                      report.append(Record(  device=device, guestName = lcomp.guestName, guestUrl = guestUrl, guestCpuCount = lcomp.guestCpuCount, guestMemory = lcomp.guestMemoryCurrent)) 
+                  report.append(Record( device=device, deviceName=device.titleOrId(), guestName = lcomp.guestName, guestUrl = guestUrl, guestCpuCount = lcomp.guestCpuCount, guestMemory = lcomp.guestMemoryCurrent)) 
 
         return report
